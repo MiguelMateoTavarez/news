9633febc2706e42bd14dd6dfb0eadea86d7ae7bd
Miguel Mateo <miguelmateot94@gmail.com>
2024-04-12 19:06:41 -0400

    Add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import logo from '../assets/logo/1.png';
 
 export const Navbar = () => {
     return (
@@ -6,12 +7,12 @@ export const Navbar = () => {
             <Toolbar>
                 {/* Logo a la izquierda */}
                 <IconButton edge="start" color="inherit" aria-label="menu">
-                    <img src='' alt="Logo" style={{ width: 40, marginRight: 10 }} />
+                    <img src={logo} alt="Logo" style={{ width: 50, marginRight: 10 }} />
                 </IconButton>
 
                 {/* Nombre del blog en el centro */}
-                <Typography variant="h6" style={{ flexGrow: 1, textAlign: 'center' }}>
-                    Nombre del Blog
+                <Typography variant="h3" style={{ flexGrow: 1, textAlign: 'center' }}>
+                    EM News
                 </Typography>
 
                 {/* Botón "Suscríbete" a la derecha */}
