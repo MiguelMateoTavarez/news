0feb9960d88fded4c865d59f72d38dec5ca04d0f
Miguel Mateo <miguelmateot94@gmail.com>
2024-04-12 19:02:41 -0400

    Implement Card

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,37 +1,50 @@
 // src/components/NewsCard.js
 
 import styled from "@emotion/styled";
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 
 
 
 // Estilos personalizados para la card de noticia
 const useStyles = styled((theme) => ({
   card: {
-    marginBottom: theme.spacing(2), // Espaciado inferior entre cards
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    position: 'relative', // Para establecer la posición relativa del Card
   },
-  media: {
-    height: 200, // Altura de la imagen de la noticia principal
+  cardContent: {
+    flexGrow: 1,
+  },
+  cardActions: {
+    position: 'absolute',
+    bottom: 5, // Distancia de 5px desde la parte inferior del Card
+    left: 0,
+    right: 0,
+    margin: '0 auto', // Centra el CardActions horizontalmente
   },
 }));
 
-const NewsCard = ({ title = 'Default title', imageUrl='', description='Default description', large=true }) => {
-
+const NewsCard = ({ title = 'Default title', imageUrl = '', description = 'Default description', large = true }) => {
+  console.log(imageUrl)
   return (
     <Card className={useStyles.card}>
-        {/* add image */}
-      {/* <CardMedia
-        className={useStyles.media}
+      <CardMedia
+        component="img"
+        height="194"
         image={imageUrl}
-        title={title}
-      /> */}
-      <CardContent>
+        alt="Paella dish"
+      />
+      <CardContent className={useStyles.cardContent}>
         <Typography variant={large ? "h5" : "h6"} component="h2">
           {title}
         </Typography>
         <Typography variant="body2" color="textSecondary" component="p">
           {description}
         </Typography>
+        <CardActions className={useStyles.cardActions}>
+          <Button variant="contained">Leer más</Button>
+        </CardActions>
       </CardContent>
     </Card>
   );
--- src/journal/HomeJournal.jsx
+++ src/journal/HomeJournal.jsx
@@ -1,8 +1,10 @@
 import { Navbar } from "../components/Navbar"
 import NewsCard from "../components/NewsCard"
-// import newsImage1 from './assets/news1.jpg';
-// import newsImage2 from './assets/news2.jpg';
-// import newsImage3 from './assets/news3.jpg';
+import newsImage1 from '../assets/news/mexico_rompe.jpeg';
+import newsImage2 from '../assets/news/palacio.jpg';
+import newsImage3 from '../assets/news/banderas.avif';
+import newsImage4 from '../assets/news/homeland_sec.webp';
+import newsImage5 from '../assets/news/taiwan.png';
 
 export const HomeJournal = () => {
   return (
@@ -10,21 +12,33 @@ export const HomeJournal = () => {
       <Navbar />
       <div style={{ padding: '20px' }}>
         <NewsCard
-          title="Noticia Principal"
-          imageUrl=""
-          description="Descripción de la noticia principal."
+          title="México rompe relaciones diplomáticas con Ecuador"
+          imageUrl={newsImage1}
+          description="Fuerzas especiales de la policía ecuatoriana intentan irrumpir en la embajada de México en Quito para detener al exvicepresidente de Ecuador Jorge Glas, el 5 de abril de 2024..."
           large
         />
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ display: 'flex', gap: 8, justifyContent: 'space-between', marginTop: '10px' }}>
           <NewsCard
-            title="Noticia Complementaria 1"
-            imageUrl=""
-            description="Descripción de la primera noticia complementaria."
+            title="R.D rechaza irrupción en embajada mexicana en Ecuador"
+            imageUrl={newsImage2}
+            description=""
           />
           <NewsCard
-            title="Noticia Complementaria 2"
-            imageUrl="{newsImage3}"
-            description="Descripción de la segunda noticia complementaria."
+            title="RD colaborará en salida de ciudadanos de USA de Haití"
+            imageUrl={newsImage3}
+            description=""
+          />
+        </div>
+        <div style={{ display: 'flex', gap: 8, justifyContent: 'space-between', marginTop: '10px' }}>
+          <NewsCard
+            title="Haitianos que lleguen a USA por mar, serán repatriados"
+            imageUrl={newsImage4}
+            description=""
+          />
+          <NewsCard
+            title="Réplicas de terremoto en Taiwan, detienen demolición"
+            imageUrl={newsImage5}
+            description=""
           />
         </div>
       </div>
